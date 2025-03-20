0e1b769edd3e746949635a3dae6a7ed1d636f702
abooli1952 <abooli1952@gmail.com>
2025-03-20 18:39:49 +0530

    20march

---
+++ js.js
@@ -0,0 +1,8 @@
+function func()
+            {
+                document.write("Welcome ");
+            }
+function func1()
+            {
+                document.write(" to website");
+            }
